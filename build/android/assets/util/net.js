6f84a4bf4d7389fa846cc2ed1ee8ab98c80887c1
weixingsun <weixing.sun@gmail.com>
2015-07-06 00:28:02 +1200

    commit 2: detect network and use include to separate logic codes

---
+++ build/android/assets/util/net.js
@@ -0,0 +1,1 @@
+if(Titanium.Network.networkType==Titanium.Network.NETWORK_NONE){var alertDialog=Titanium.UI.createAlertDialog({title:"WARNING!",message:"Your device is not online.",buttonNames:["OK"]});alertDialog.show()}else{var alertDialog=Titanium.UI.createAlertDialog({title:"Good!",message:"Your device is online.",buttonNames:["OK"]});alertDialog.show()}
