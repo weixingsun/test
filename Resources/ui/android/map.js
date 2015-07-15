6b2bbe1141d55fe48bf981aba5364d5a73823e0c
weixingsun <weixing.sun@gmail.com>
2015-07-15 15:14:26 +1200

    fix: addFileLayer NPE possible solution: create view complete event.

--- Resources/ui/android/map.js
+++ Resources/ui/android/map.js
@@ -1,62 +1,75 @@
-var ALL = {
-	Marker:{"dest":0},
-	Gps:{"lat":0,"lng":0,"heading":0,"accuracy":0,"speed":0},
-	Line:{"route":0},
-	Nodes:{}
-};
+var ALL = initVar();
+var win = initWindow();
+var mf = initModule();
+var map = initMap(win,mf);
+initNav(mf);
+addListeners();
+addOfflineMapLayer(map);
+hideBar(win);
 
-var win = Ti.UI.createWindow({
-	backgroundColor:'white'
-});
-var mf = require('ti.mapsforge');
-mf.load("/osmdroid/maps/nz/");
-var mapView = mf.createMapsforgeView({
- "scalebar": true,
- "minZoom": 5, //Min zoom level for map view
- "maxZoom": 20, //Max zoom level for map view
- "centerLatlng": [-43.524551, 172.58346], //locke
- "zoomLevel": 12, //Bogus initial zoom level
- "debug": false });
-
-win.add(mapView);
-win.open();
-Ti.API.info('mapView: ' + JSON.stringify(mapView));
-
-Ti.App.addEventListener('clicked', function(e) {
-	var resid = Ti.App.Android.R.drawable.marker_tap;
-	alert('clicked('+e.lat+','+e.lng+')');
-  /*mapView.createMarker({
-	"iconPath": "/images/marker_tap.png",
-	"latlng": [e.lat, e.lng]
-  });*/
-});
-Ti.App.addEventListener('longclicked', function(e) {
-	Ti.API.info('longclicked('+e.lat+','+e.lng+')');
-	var from = [-43.524551, 172.58346];
-	var to = [e.lat,e.lng];
-	navi(from,to);
-	removePrevDestMarker();
-	marker(to);
-});
-//mapView.centerLatlng = [-43.524551, 172.58346];
-//mapView.zoomLevel = 12;
-/*mapView.addLayer({
-	"name": "osm",
-	"url": "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-	"subdomains": ["a", "b"],
-	"parallelRequests": 4,
-	"maxZoom": "20",
-	"minZoom": "4"
-});
-*/
- mapView.addLayer({
-	"name": "osm",
-	"path": "osmdroid/maps/nz/nz.map",
-});
-
-var actionBar = win.activity.actionBar;
-if(typeof actionBar != 'undefined'){
-	actionBar.hide();
+function initWindow(){
+	return Ti.UI.createWindow();
+}
+function initModule(){
+	return require('ti.mapsforge');
+}
+function initVar(){
+	var ALL = {
+		Marker:{"dest":0},
+		Gps:{"lat":0,"lng":0,"heading":0,"accuracy":0,"speed":0},
+		Line:{"route":0},
+		Nodes:{}
+	};
+	return ALL;
+}
+function hideBar(win){
+	var actionBar = win.activity.actionBar;
+	if(typeof actionBar !== 'undefined'){
+		actionBar.hide();
+	}
+}
+function initMap(win,module){
+	var mapView = module.createMapsforgeView({
+	 "scalebar": true,
+	 "minZoom": 5, //Min zoom level for map view
+	 "maxZoom": 20, //Max zoom level for map view
+	 "centerLatlng": [-43.524551, 172.58346], //locke
+	 "zoomLevel": 12, //Bogus initial zoom level
+	 "debug": false });
+	//mapView.centerLatlng = [-43.524551, 172.58346];
+	//mapView.zoomLevel = 12;
+	//Ti.API.info('mapView: ' + JSON.stringify(mapView));
+	win.add(mapView);
+	win.open();
+	Ti.API.info('mapView: ' + JSON.stringify(mapView));
+	return mapView;
+}
+function addOfflineMapLayer(map){
+	map.addLayer({
+		"name": "osm",
+		"path": "osmdroid/maps/nz/nz.map",
+	});
+}
+function initNav(mf){
+	//load graphhopper folder
+	mf.load("/osmdroid/maps/nz/");
+}
+function addListeners(){
+	Ti.App.addEventListener('clicked', function(e) {
+		var resid = Ti.App.Android.R.drawable.marker_tap;
+		var point=[e.lat,e.lng];
+	    //var poiPoint = findPOI(point,radius);
+	    //addMarker(poiPoint);
+	    //openPopup();
+	});
+	Ti.App.addEventListener('longclicked', function(e) {
+		Ti.API.info('longclicked('+e.lat+','+e.lng+')');
+		var from = [-43.524551, 172.58346];
+		var to = [e.lat,e.lng];
+		navi(from,to);
+		removePrevDestMarker();
+		addMarker(to);
+	});
 }
 
 function removePrevDestMarker(){
@@ -65,7 +78,7 @@ function removePrevDestMarker(){
 		mapView.removeLayer(pre_marker);
 	}
 }
-function marker(to){
+function addMarker(to){
 	//platform/android/res/drawable/marker_tap.png
 	var dest = mapView.createMarker({
 		"iconPath": Ti.App.Android.R.drawable.marker_tap,
@@ -98,63 +111,3 @@ function navi(from,to){
 		}
 	});
 }
-
-//Ti.include('marker.js');
-/*
-//Draw a blue line on the map...
-var polyline = mapView.createPolyline({
-	"latlngs": [
-		[-43.524551, 172.58346],
-		[-43.524551, 172.59346],
-		[-43.534551, 172.58346]
-	], 
-	"color": "blue",
-	"strokeWidth": 5
-});
-//Ti.API.info('Created polyline: ' + JSON.stringify(polyline));
-polyline.color = "red";
-polyline = mapView.updateLayer(polyline);
-//Note, that your previous reference is invalid and has
-//to be replaced with the new one returned from updateLayer()
-//Ti.API.info('Updated polyline to: ' + JSON.stringify(polyline));
-
-//Draw a green polygon with a thick black stroke
-mapView.createPolygon({
-	"latlngs": [
-		[-43.524551, 172.58346],
-		[-43.524551, 172.60346],
-		[-43.544551, 172.60346],
-		[-43.544551, 172.58346],
-		[-43.524551, 172.58346]
-	], 
-	"fillColor": "green",
-	"strokeColor": "black",
-	"strokeWidth": 5}
-);
-//Draw a marker at the same position as above but with offset
-mapView.createMarker({
-	"iconPath": "http://www.google.com/mapfiles/dd-start.png",
-	"latlng": [-43.524551, 172.58346],
-	"hOffset": 5,
-	"vOffset": 4
-	});
-
-//Draw a sized marker (of the Zuck) Original icon is 100x99 pixels
-//temporary broken: since read from file to inputstream method not implemented in module: ti.mapsforge
-mapView.createMarker({
-	"iconPath": "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn2/202896_4_1782288297_s.jpg",
-	"latlng": [-43.529551, 172.63346],
-	"iconSize": [64, 64] //New size in pixels
-});
-*/
-//Draw a circle
-/*
-mapView.createCircle({
-	"latlng": [-43.529551, 172.63346],
-	"fillColor": "blue",
-	"strokeColor": "red",
-	"radius": 500 //This is meters!
-});
-
- * 
- * */
