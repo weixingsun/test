275fc5d83df2a74069c81c3f34f8a828e0edee68
weixingsun <weixing.sun@gmail.com>
2015-07-25 17:44:33 +1200

    change ceil to floor when calculating distance to next turn

--- Resources/util/audio.js
+++ Resources/util/audio.js
@@ -4,7 +4,7 @@ var player = Titanium.Media.createSound();
 function play(type,dist){
 	var GH = getGHDict();
 	var signname = GH[type];
-	var dist00 = Math.ceil(dist/100)*100;//ceil/floor/round
+	var dist00 = Math.floor(dist/100)*100;//ceil/floor/round
 	var file = signname+'_dist_'+dist00+'.mp3';
 	var path = Ti.Filesystem.getResRawDirectory() + file;
 	//var inFile = Ti.Filesystem.getFile(path);
