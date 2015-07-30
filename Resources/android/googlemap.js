a0357a92746080c3b5da6f21765ca7b96c896b21
weixingsun <weixing.sun@gmail.com>
2015-07-31 00:29:31 +1200

    enabled google map after shrinking the size of graphhopper.trove4j jar file. auto choose map types when starting up the app: online->google, offline->mapsforge

--- Resources/android/googlemap.js
+++ Resources/android/googlemap.js
@@ -1,2 +1,50 @@
+var module = require('ti.map');
+//var mapview = module.createView({mapType:module.NORMAL_TYPE});
 
-var MapModule = require('ti.map');
+var win = Ti.UI.createWindow({backgroundColor: 'white'});
+var map1 = module.createView({
+    userLocation: true,
+    mapType: module.NORMAL_TYPE,
+    animate: true,
+    region: {latitude: -33.87365, longitude: 151.20689, latitudeDelta: 0.1, longitudeDelta: 0.1 },
+    height: '50%',
+    top: 0,
+    left: 0,
+    width: '50%'
+});
+var map2 = module.createView({
+    userLocation: true,
+    mapType: module.TERRAIN_TYPE,
+    animate: true,
+    region: {latitude: -33.87365, longitude: 151.20689, latitudeDelta: 0.1, longitudeDelta: 0.1 },
+    height: '50%',
+    top: 0,
+    right: 0,
+    width: '50%'
+});
+var map3 = module.createView({
+    userLocation: true,
+    mapType: module.SATELLITE_TYPE,
+    animate: true,
+    region: {latitude: -33.87365, longitude: 151.20689, latitudeDelta: 0.1, longitudeDelta: 0.1 },
+    height: '50%',
+    bottom: 0,
+	left: 0,
+    width: '50%'
+});
+var map4 = module.createView({
+    userLocation: true,
+    mapType: module.HYBRID_TYPE,
+    animate: true,
+    region: {latitude: -33.87365, longitude: 151.20689, latitudeDelta: 0.1, longitudeDelta: 0.1 },
+    height: '50%',
+    bottom: 0,
+    right: 0,
+    width: '50%',
+    traffic: true
+});
+win.add(map1);
+win.add(map2);
+win.add(map3);
+win.add(map4);
+win.open();
--- Resources/app.js
+++ Resources/app.js
@@ -12,28 +12,35 @@ if(Ti.Platform.osname = "android"){
 	Ti.include('util/poi.js');
 	Ti.include('util/audio.js');
 	Ti.include('util/saved.js');
-	var mapPath="map/nz.map";
-	if(Ti.Filesystem.isExternalStoragePresent()){
-        var file = Ti.Filesystem.getFile(Ti.Filesystem.externalStorageDirectory, mapPath);
-	}else {// No SD
-		Ti.API.info('applicationDataDirectory='+Ti.Filesystem.applicationDataDirectory+', path='+mapPath);
-        var file = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, mapPath);
-    }
-	if(file.exists()){
-		Ti.API.info('use offline map');
-		Ti.include('mapsforge.js');
+	//////////////////////////////////////////////////////////////////////////
+	if(Ti.Network.networkType == Ti.Network.NETWORK_NONE){
+		var mapPath="map/nz.map";
+		if(Ti.Filesystem.isExternalStoragePresent()){
+	        var file = Ti.Filesystem.getFile(Ti.Filesystem.externalStorageDirectory, mapPath);
+		}else {// No SD
+			Ti.API.info('applicationDataDirectory='+Ti.Filesystem.applicationDataDirectory+', path='+mapPath);
+	        var file = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, mapPath);
+	    }
+		if(file.exists()){
+			Ti.API.info('use offline map');
+			Ti.include('mapsforge.js');
+		}else{
+			Ti.API.info('use online google map');
+			Ti.include('googlemap.js');
+		}
 	}else{
 		Ti.API.info('use online google map');
 		Ti.include('googlemap.js');
 	}
 }else{
+	//iOS
 	var mapPath="map/nz.map";
 	var file = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, mapPath);
 	if(file.exists()){
-		Ti.API.info('use offline map');
-		Ti.include('ios/mapsforge.js');
+		Ti.API.info('use offline map in ios');
+		Ti.include('mapsforge.js');
 	}else{
-		Ti.API.info('use online google map');
-		Ti.include('ios/googlemap.js');
+		Ti.API.info('use online google map in ios');
+		Ti.include('googlemap.js');
 	}
 }
